c861514006822d9e20d39b34ef50adeb6f6954c4
inamnurul <aam.inam7310@gmail.com>
2022-11-21 20:11:21 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,99 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet/dist/leaflet';
+import 'leaflet-draw/dist/leaflet.draw.css'
+import 'leaflet-draw/dist/leaflet.draw'
 
-function App() {
+import { useEffect, useState } from 'react';
+
+
+
+function Map() {
+  const [data, setData]= useState('')
+  const [title,setTitle]=useState('')
+  const [description,setDescription]=useState('')
+
+  const handleSubmit= async (e)=>{
+    const mission = {title,data,description}
+
+    const response =await fetch('http://localhost:4000/mission',{
+      method: 'POST',
+      body: JSON.stringify(mission),
+      headers:{
+        'Content-Type': 'application/json'
+      }
+    })
+    const json= await response.json()
+
+    if (!response.ok){
+      console.log(json.error)
+    }
+    if(response.ok){
+      console.log("u made a mission :",json)
+    }
+
+  }
+  var dataa=""
+  useEffect(() => {
+  
+    const map = L.map('mapid', {
+      center: [-7.770905, 110.377637],
+      zoom: 18
+    })
+
+    L.tileLayer(
+      'https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 19,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }
+    ).addTo(map);
+
+    const drawnItems = new L.FeatureGroup()
+    map.addLayer(drawnItems);
+    const drawControl = new L.Control.Draw({
+      polygon: {
+        showArea: true
+      },
+      edit: {
+        featureGroup: drawnItems
+      }
+    });
+    map.addControl(drawControl);
+
+    map.on(L.Draw.Event.CREATED, function (event) {
+      drawnItems.addLayer(event.layer);
+      dataa = JSON.stringify(drawnItems.toGeoJSON());
+      setData(dataa);
+      console.log(dataa);
+    });
+    map.on(L.Draw.Event.EDITED, function (event) {
+      dataa = JSON.stringify(drawnItems.toGeoJSON());
+      setData(dataa);
+      console.log(dataa);
+    });
+    map.on(L.Draw.Event.DELETED, function (event) {
+      dataa = JSON.stringify(drawnItems.toGeoJSON());
+      setData(dataa);
+      console.log(dataa);
+    });
+
+    return () => {
+      map.off()
+      map.remove()
+    }
+  },[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div className='map-container' >
+        <div id='mapid' style={{ height: '100vh' }}></div>
+      </div>
+      <input type={'text'} onChange={(e)=>{setTitle(e.target.value)}}>
+      </input>
+      <button onClick={handleSubmit}> SAVE </button>
+
+    </>
+  )
 }
 
-export default App;
+export default Map
--- src/index.js
+++ src/index.js
@@ -3,11 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Listmission from './listmission';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <Listmission/>
   </React.StrictMode>
 );
 
